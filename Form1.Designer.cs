83e3ed46406618f0e15dc24fe3d47a7930c21561
artem <artem@DESKTOP-QDHVL2C>
2020-05-25 13:58:57 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Urok_5___1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.File = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Folder = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // File
+            // 
+            this.File.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.File.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.File.Location = new System.Drawing.Point(182, 155);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(115, 31);
+            this.File.TabIndex = 1;
+            this.File.Text = "Открыть файл";
+            this.File.UseVisualStyleBackColor = false;
+            this.File.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.progressBar1.Location = new System.Drawing.Point(182, 232);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(407, 18);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.UseWaitCursor = true;
+            this.progressBar1.Visible = false;
+            // 
+            // Folder
+            // 
+            this.Folder.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Folder.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Folder.Location = new System.Drawing.Point(474, 155);
+            this.Folder.Name = "Folder";
+            this.Folder.Size = new System.Drawing.Size(115, 31);
+            this.Folder.TabIndex = 3;
+            this.Folder.Text = "Открыть папку";
+            this.Folder.UseVisualStyleBackColor = false;
+            this.Folder.Click += new System.EventHandler(this.Folder_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 35;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 35;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(313, 326);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Введите имя файла";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(330, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Создать файл";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(330, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Удалить файл";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Folder);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.File);
+            this.Name = "Form1";
+            this.Text = "Object Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void FolderBrowserDialog1_Disposed(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button File;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button Folder;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Urok_5___1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                progressBar1.Visible = false;
+                FileInfo file = new FileInfo(openFileDialog1.FileName);
+                label1.Text = file.FullName;
+                MessageBox.Show($"{file.Name}\n{file.CreationTime}\n{file.Attributes}", "Files data") ;
+            }
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            progressBar1.Visible = true;
+            timer1.Enabled = true;
+        }
+
+        private void Folder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                progressBar1.Visible = true;
+                timer2.Enabled = true;
+            }
+        }
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                timer2.Enabled = false;
+                progressBar1.Visible = false;
+                DirectoryInfo dir = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                label1.Text = dir.FullName;
+                var files = dir.GetFiles();
+                string txt = "";
+                foreach (FileInfo file in files)
+                {
+                    txt = txt + file.Name + "\n";
+                }
+                MessageBox.Show(txt,"Files in the directory");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = folderBrowserDialog1.SelectedPath + @"\" + textBox1.Text;
+            FileInfo file = new FileInfo(path);
+            FileStream stream = file.Create();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string path = folderBrowserDialog1.SelectedPath + @"\" + textBox1.Text;
+            FileInfo file = new FileInfo(path);
+            file.Delete();
+        }
+    }
+}
